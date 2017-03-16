cb292cdbc890ceaf3389b80e5b4c4c10b2c546c0
Allan Carvalho <allan.carvalho@b2wdigital.com>
2017-03-16 11:47:11 -0300

    correcao de bug

--- inject/stash.js
+++ inject/stash.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
-	var nome = 'Allan';
-	var tags = $('.tag.aggregate').data('names').split(':');
-	const html = tags.map(tag => { 
-		return `<span class="tag" style="display:inline-block; margin-left: 5px;" data-names="${tag}" original-title=""><span class="aui-icon aui-icon-small aui-iconfont-devtools-tag-small"></span><a href="/projects/BPDP/repos/product-page/commits?until=${tag}">${tag}</a></span>`;
+	$('.tag.aggregate').map((index, tag) => {
+		const tags = $(tag).data('names').split(':');
+		const html = tags.map(tag => { 
+			return `<span class="tag" style="display:inline-block; margin-left: 5px;" data-names="${tag}" original-title=""><span class="aui-icon aui-icon-small aui-iconfont-devtools-tag-small"></span><a href="/projects/BPDP/repos/product-page/commits?until=${tag}">${tag}</a></span>`;
+		});
+		$(tag).parent().html(html);
 	});
-	$('.tag.aggregate').parent().html(html);
 });
